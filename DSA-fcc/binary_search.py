d8593beb4233cf6888d1adefdfcee341d5ccde4a
vishalvennu <31825505+vishalvennu@users.noreply.github.com>
2024-01-18 14:10:48 -0600

    linear and binary -fcc

---
+++ DSA-fcc/binary_search.py
@@ -0,0 +1,23 @@
+# list needs to be a sorted one. 
+# we are assuming the list is ascending in the code below.
+def binary_search(list, target):
+    min = 0
+    max = len(list) - 1
+    
+    while min <= max:
+        midpoint = (min+max)//2 # need to calculate midpoint everytime after min or max changes
+        if list[midpoint] == target:
+            return midpoint 
+        elif list[midpoint] <= target:
+            min = midpoint+1
+        else:
+            max = midpoint-1
+    return "Not found"
+
+list = [1, 2, 3, 5, 8, 12, 23, 54, 89]
+target = 12
+
+print(binary_search(list,target))
+
+
+    
