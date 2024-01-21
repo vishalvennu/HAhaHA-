7fcacf18f41a437a2d9326e1fffe3ce184131dd3
vishalvennu <vv819@nyu.edu>
2024-01-20 21:52:55 -0600

    widest completed

--- leetcode-150/widestVerticalDistance.py
+++ leetcode-150/widestVerticalDistance.py
@@ -7,13 +7,18 @@ def widestVerticalDistance(points):
         xlist.append(point[0])
     
     xlist.sort()
-    xlen = len(xlist)
+    xlast = len(xlist)-1
+    xgap = 0
 
-    while xlen > 0:
-        
+    while xlast > 0:
+        gap = xlist[xlast] - xlist[xlast-1]
+        if gap > xgap:
+            xgap = gap
+        xlast -= 1
 
+    return xgap
     
 
 
 points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
-widestVerticalDistance(points)
+print(widestVerticalDistance(points))
