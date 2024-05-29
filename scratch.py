7a1107bceb8d4d20d5c297cab14477721858426a
vishalvennu <31825505+vishalvennu@users.noreply.github.com>
2024-05-29 11:52:43 -0400

    29 May

--- scratch.py
+++ scratch.py
@@ -1,27 +1,12 @@
-def format_duration(seconds):
-    if seconds == 0 : return "now"
-    temp = {}
-    temp["years"], rem = divmod(seconds, 3600*365*24)
-    temp["days"], rem = divmod(rem, 3600*24)
-    temp["hours"], rem = divmod(rem, 3600)
-    temp["minutes"], temp["seconds"] = divmod(rem, 60)
-    
-    # format_output(years = years, days = days, hours = hours, minutes = minutes, seconds = secs)
-    # print(f"{years} years {days} days {hours} hours {minutes} minutes and {secs} seconds",)
-    
-    temp = {key:value for key,value in temp.items() if value != 0}
-    print(temp)
-    
-    l = None
-    if len(temp.keys()) > 1:
-        l = list(temp.keys())[-2]
-    output = ""    
-    for key,value in temp.items():
-        if value == 1:
-            output += str(value)+" "+key[:-1]+" "
+def duplicate_count(text):
+    # Your code goes here
+    register = []
+    count = 0
+    for i in text:
+        if i not in register:
+            register.append(i)
         else:
-            output += str(value)+" "+key+" "
-        if key == l:
-            output += "and "
-    print(output[:-1])
-format_duration(341201)
+            count += 1
+    return count
+
+print(duplicate_count("Lambidii"))
